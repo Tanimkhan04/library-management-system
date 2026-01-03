3d9a0c6136701bedf8000247461355febcad0730
Tanimkhan04 <tanim.khan.60@gmail.com>
2026-01-04 03:11:29 +0600

    Initial commit

---
+++ View/homepage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Student Home</title>
+</head>
+<body>
+
+<h1>Student Home Page</h1>
+<p>Welcome to the Library Management System</p>
+
+<a href="login.html">Login</a> |
+<a href="register.html">Register</a>
+
+</body>
+</html>
