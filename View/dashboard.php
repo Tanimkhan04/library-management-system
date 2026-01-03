68640e794d17b183e205e3119f261b69ae7379f7
Tanimkhan04 <tanim.khan.60@gmail.com>
2026-01-04 03:14:54 +0600

    Initial commit

---
+++ View/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Student Dashboard</title>
+</head>
+<body>
+
+<h1>Student Dashboard</h1>
+
+<ul>
+  <li><a href="search.html">Search Books</a></li>
+  <li><a href="borrowed.html">View Borrowed Books</a></li>
+  <li><a href="return.html">Return Book</a></li>
+  <li><a href="wishlist.html">Wishlist</a></li>
+  <li><a href="review.html">Rate & Review</a></li>
+</ul>
+
+<a href="logout.html">Logout</a>
+
+</body>
+</html>
---
+++ View/logout.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Logout</title>
+</head>
+<body>
+
+<p>You have successfully logged out.</p>
+<a href="login.html">Login Again</a>
+
+</body>
+</html>
