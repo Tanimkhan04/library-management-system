e7389d567d3c7258529de9a5f56f9219426eb868
Tanimkhan04 <tanim.khan.60@gmail.com>
2026-01-04 14:10:07 +0600

    Initial commit

---
+++ View/search_request.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Search Book</title>
+</head>
+<body>
+
+<h1>Search Book</h1>
+
+<input type="text" placeholder="Enter book name">
+<br><br>
+<button>Search</button>
+<br><br>
+<button>Request Book</button>
+
+</body>
+</html>
---
+++ View/view_book.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Borrowed Books</title>
+</head>
+<body>
+
+<h1>Borrowed Books</h1>
+
+<p>Book Name: Web Technology</p>
+<p>Due Date: 15-01-2026</p>
+
+</body>
+</html>
