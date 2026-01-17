87311b4a86bbe13d15dd86f4dbb5f1c0c887cb1b
Tanimkhan04 <tanim.khan.60@gmail.com>
2026-01-18 00:04:43 +0600

    Initial commit

--- Management/Student/MVC/Control/logout.php
+++ Management/Student/MVC/Control/logout.php
@@ -4,3 +4,25 @@ session_start();
 session_destroy();
 header("Location: login.php");
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-color: lightblue;
+  font-family: verdana;
+}
+
+p {
+  font-size: 20px;
+  text-align: center;
+  color: darkgreen;
+}
+</style>
+</head>
+
+<body>
+<p>You have successfully logged out.</p>
+
+</body>
+</html>
