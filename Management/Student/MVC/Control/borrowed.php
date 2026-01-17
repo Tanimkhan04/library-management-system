c8488e681194fa4d8f0fd7f60b39db4a7686d5d2
Tanimkhan04 <tanim.khan.60@gmail.com>
2026-01-17 23:54:00 +0600

    Initial commit

--- Management/Student/MVC/Control/borrowed.php
+++ Management/Student/MVC/Control/borrowed.php
@@ -7,13 +7,26 @@ include("config.php");
 <html>
 <head>
   <title>Borrowed Books</title>
+  <style>
+body {
+  background-color: lightblue;
+  font-family: verdana;
+}
+
+p {
+  font-size: 18px;
+}
+
+span {
+  color: red;
+}
+</style>
 </head>
 <body>
 
 <h1>Borrowed Books</h1>
 
 <p><b>Book:</b> Web Technology</p>
-
 <p><b>Due Date:</b> 20-01-2026</p>
 
 <a href="dashboard.php">Back</a>
