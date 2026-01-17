ae88593f8ace29620a4f7b9341b1a82bba4faa09
Tanimkhan04 <tanim.khan.60@gmail.com>
2026-01-18 00:12:31 +0600

    Initial commit

--- Management/Student/MVC/Control/review.php
+++ Management/Student/MVC/Control/review.php
@@ -12,6 +12,46 @@ if (isset($_POST['submit'])) {
 <html>
 <head>
   <title>Rate & Review</title>
+  <style>
+body {
+  background-color: lightblue;
+  font-family: verdana;
+}
+
+div {
+  background-color: white;
+  width: 320px;
+  padding: 20px;
+  margin: auto;
+  border-radius: 10px;
+}
+
+h2 {
+  color: darkblue;
+  text-align: center;
+}
+
+textarea {
+  width: 100%;
+  height: 80px;
+  margin-top: 10px;
+  padding: 8px;
+}
+
+button {
+  margin-top: 10px;
+  width: 100%;
+  background-color: darkgreen;
+  color: white;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+}
+
+button:hover {
+  background-color: green;
+}
+</style>
 </head>
 <body>
 
