df2bd2447de59832ffce683626056a315e1be013
Tanimkhan04 <tanim.khan.60@gmail.com>
2026-01-19 20:10:00 +0600

    Initial commit

--- Management/Student/MVC/sql/database.php
+++ Management/Student/MVC/sql/database.php
@@ -6,9 +6,9 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
     email VARCHAR(100),
-    password VARCHAR(255),
-    role VARCHAR(20)
+    password VARCHAR(100),
+    
 );
-INSERT INTO users (name, email, password, role)
-VALUES ('Student One', 'Tanim.khan60@gmail.com', '1234', 'student');
+INSERT INTO users (name, email, password)
+VALUES ('Student One', 'Tanim.khan60@gmail.com', '1234');
 
